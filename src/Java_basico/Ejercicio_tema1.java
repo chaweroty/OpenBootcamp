5ae94d43889f267bd9144222f5e6e0798a643ac8
Sebastián García <smasgg@gmail.com>
2022-12-30 19:51:48 -0500

    new

---
+++ src/Java_basico/Ejercicio_tema1.java
@@ -0,0 +1,18 @@
+package Java_basico;
+
+public class Ejercicio_tema1 {
+    public static void main(String[] args) {
+
+        int numero = 12;
+        long numero2 = 123234;
+        double numero3 = 1234;
+        boolean status = false;
+        String nombre = "Sebastian";
+
+        System.out.println(numero);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(status);
+        System.out.println(nombre);
+    }
+}
